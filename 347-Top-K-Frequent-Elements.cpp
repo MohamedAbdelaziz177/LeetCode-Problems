1f92512763c6ff95bdeb7265ca469b5379e9511b
Mohamed Abdelaziz <145907767+MohamedAbdelaziz177@users.noreply.github.com>
2025-01-06 23:26:39 +0200

    [347] [Time Beats: 67.28%] [Memory Beats: 68.49%] - LeetPush

---
+++ 347-Top-K-Frequent-Elements.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& v, int k) {
+        
+        map<int, int>mp;
+        priority_queue<pair<int, int>>vp;
+
+        for(int i = 0; i < v.size(); i++)
+        mp[v[i]]++;
+
+        for(auto el : mp)
+        {
+            vp.push({el.second, el.first});
+        }
+
+     
+
+        vector<int>vv;
+
+        int i = 0;
+
+        while(i++ < k)
+        {
+            vv.push_back(vp.top().second);
+            vp.pop();
+        }
+        
+
+        return vv;
+
+    }
+};
