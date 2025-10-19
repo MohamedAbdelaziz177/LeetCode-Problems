10a1d649e2bdadc19c9057f9244835ab8691b0bc
Mohamed Abdelaziz <145907767+MohamedAbdelaziz177@users.noreply.github.com>
2025-10-19 11:05:48 +0300

    [210] [Time Beats: 33.10%] [Memory Beats: 56.96%] - LeetPush

---
+++ Course-Schedule-II.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool visited[2005];
+    int inDegree[2005];
+    vector<int>graph[2005];
+
+    vector<int> bfs_topo(int n)
+    {
+        queue<int>q;
+        vector<int>v;
+        
+        for(int i = 0; i < n; i++)
+        if(!inDegree[i])
+        {
+            q.push(i);
+            v.push_back(i);
+        }
+
+
+        while(!q.empty())
+        {
+            auto node = q.front();
+            q.pop();
+
+            for(auto x : graph[node])
+            {
+                inDegree[x]--;
+                if(!inDegree[x])
+                {
+                    q.push(x);
+                    v.push_back(x);
+                }
+            }
+        }
+
+        return v;
+    }
+    vector<int> findOrder(int n, vector<vector<int>>& v) {
+        for(auto e : v)
+        {
+            graph[e[1]].push_back(e[0]);
+            inDegree[e[0]]++;
+        }
+
+        vector<int> ans = bfs_topo(n);
+        
+        if(ans.size() != n)
+            ans.resize(0);
+        
+        return ans;
+    }
+};
